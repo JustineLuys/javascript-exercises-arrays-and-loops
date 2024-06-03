a71ef53e827e873920a8d09e7cc4173963bbacc4
Justine Paul <justineluyon9@gmail.com>
2024-06-03 20:25:45 +0800

    exercise 1-6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...items) {
+    return array.filter((item) => !items.includes(item));
 
+//    return array.filter((item) => {
+//         for (let i = 0; i < args.length; i++) {
+//             if (item === args[i]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     });
+};
 // Do not edit below this line
 module.exports = removeFromArray;
+
