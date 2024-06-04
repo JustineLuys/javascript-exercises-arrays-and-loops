fb68eb66e51603512de0e697e6050ad7f54af5a9
Justine Paul <justineluyon9@gmail.com>
2024-06-04 09:13:08 +0800

    Exercises 1-9 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,5 @@
 const leapYears = function(year) {
-    // return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
-        if (year % 4 === 0) {
-            if (year % 100 === 0) {
-                return year % 400 === 0;
-                } else {
-                    return true;
-                }
-            } else {
-                return false;
-            }
+     return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
     }
 //    if (year % 100 === 0) {
 //         if (year % 400 === 0) {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
-
-// Do not edit below this line
-module.exports = palindromes;
+const palindromes = function (string) {
+    // Convert to lowercase, remove non-alphanumeric characters using regex, then rejoin as new string
+    const cleanedString = string
+      .toLowerCase()
+      .replace(/[\W_]/g, '');
+  
+    // Create a new reversed string for comparison
+    const reversedString = cleanedString.split('').reverse().join('');
+    console.log(cleanedString);
+    console.log(reversedString);
+    // Return the outcome of the comparison which will either be true or false
+    return cleanedString === reversedString;
+  };
+  console.log(palindromes('A car, a man, a maraca_!.'));
+  module.exports = palindromes;
+  
