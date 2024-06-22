57f97d7d3b953f246a9679eb5cd998dc38ee4a42
Justine Paul <justineluyon9@gmail.com>
2024-06-22 21:24:20 +0800

    aa

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let array = [];
+    let firstPrev = 0, secondPrev = 1;
+    for (let i = 2; i <= num; i++) {
+        let current = firstPrev + secondPrev;
+        firstPrev = current;
+        array.push(current);
+    }
+    console.log(array);
 };
 
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const date = new Date().getFullYear();
+    const oldestPerson = people.sort((firstPerson, nextPerson) => {
+    const firstPersonAge = date - firstPerson.yearOfBirth;
+    const secondPersonAge = date - nextPerson.yearOfBirth;
+    return firstPersonAge < secondPersonAge ? 1 : -1;
+});
+return oldestPerson[0];
 };
 
 // Do not edit below this line
